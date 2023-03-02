d68664a0f8789c7d1d2ec0802c92610aeddb6905
zhoujinghui <neo350942>
2023-03-02 15:18:05 +0800

    feat(basic): add list op and unit test

---
+++ basic/ops.py
@@ -0,0 +1,26 @@
+from typing import List, Tuple, Dict, Set
+
+def get_list_len(l: List):
+    l_out = None
+    # 请在下一行编写代码，获取列表l的长度
+
+    return l_out
+
+def get_list_append_1(l: List):
+    # 请在下一行编写代码，在l后面增加一个元素1
+
+    return l
+
+def get_list_reverse(l: List):
+    # 请在下一行编写代码，将l的列表元素逆序
+
+    return l
+
+def get_list_exists(l:List, a):
+    ret = None
+    # 请在下一行编写代码，将判断a是否存在于l
+
+    return ret
+
+if __name__ == "__main__":
+    ret = get_list_len()
--- tests/test_variables.py
+++ tests/test_variables.py
@@ -78,23 +78,3 @@ def test_get_area():
 def test_get_str_format():
     ret = get_str_format("I am a number: ", 9)
     assert ret == "I am a number: 9"
-
-def test_get_list_len():
-    ret = get_list_len([1,2])
-    assert ret == 2
-
-def test_get_list_append_1():
-    ret = get_list_append_1([1,2])
-    assert ret == [1,2,1]
-
-def test_get_list_reverse():
-    ret = get_list_reverse([1, 2, 3, 4])
-    assert ret == [4, 3, 2, 1]
-    ret = get_list_reverse([1, 2])
-    assert ret == [2, 1]
-
-def test_get_list_exists():
-    ret = get_list_exists([1, 2, 3], 1)
-    assert ret == True
-    ret = get_list_exists([1, 2, 3], 4)
-    assert ret == False
