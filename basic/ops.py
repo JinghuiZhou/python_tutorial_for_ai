8496e761a315b8b4e8a7f1bc29fc3c25bfe1a9b8
zhoujinghui <neo350942>
2023-03-02 16:26:27 +0800

    feat(basic): add ops for dict and unit tests

--- basic/ops.py
+++ basic/ops.py
@@ -36,9 +36,34 @@ def return_large(a, b):
 
 def list_select_int(l:list, a):
     l_out = None
-    # 请在下一行编写代码，将l中所有的a取出，形成新的列表并返回
+    # 请在下一行编写代码，将l中所有的a取出，用取出的a形成新的列表并返回
 
     return l_out
+
+def get_dict_keys(d:Dict):
+    out = None
+    # 请在下一行编写代码，将d中的所有key取出，并形成set后返回
+
+    return out
+
+def get_dict_values(d:Dict):
+    out = None
+    # 请在下一行编写代码，将d中的所有value取出，并形成set后返回
+
+    return out
+
+def judge_dict_key_exists(d:Dict, k):
+    ret = None
+    # 请在下一行编写代码，判断k是否在d的key中,如果存在返回True,否则返回False
+
+    return ret
+
+def judge_dict_value_exists(d:Dict, v):
+    ret = None
+    # 请在下一行编写代码，判断k是否在d的value中,如果存在返回True,否则返回False
+
+    return ret
+
 
 if __name__ == "__main__":
     ret = get_list_len()
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -45,3 +45,23 @@ def test_list_select_int():
     assert ret == [1]
     ret = list_select_int([1, 2, 3], 4)
     assert ret == []
+
+def test_get_dict_keys():
+    ret = get_dict_keys(dict(a=1, b=2))
+    assert ret == set(["a", "b"])
+
+def test_get_dict_values():
+    ret= get_dict_values(dict(a=1, b=2))
+    assert ret == set([1,2])
+
+def test_judge_dict_key_exists():
+    ret = judge_dict_key_exists(dict(a=1, b=2), "a")
+    assert ret == True
+    ret = judge_dict_key_exists(dict(a=1, b=2), 1)
+    assert ret == False
+
+def test_judge_dict_value_exists():
+    ret = judge_dict_value_exists(dict(a=1, b=2), "a")
+    assert ret == False
+    ret = judge_dict_value_exists(dict(a=1, b=2), 1)
+    assert ret == True
