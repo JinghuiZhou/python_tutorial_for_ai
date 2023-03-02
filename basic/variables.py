bb3e54aff79374bd83ae7a712e2deb4b863c5058
zhoujinghui <neo350942>
2023-03-02 15:31:33 +0800

    chore(basic): remove some

--- basic/variables.py
+++ basic/variables.py
@@ -116,27 +116,5 @@ def get_str_format(s_in: str, number:int):
 
     return s_out
 
-def get_list_len(l: List):
-    l_out = None
-    # 请在下一行编写代码，获取列表l的长度
-
-    return l_out
-
-def get_list_append_1(l: List):
-    # 请在下一行编写代码，在l后面增加一个元素1
-
-    return l
-
-def get_list_reverse(l: List):
-    # 请在下一行编写代码，将l的列表元素逆序
-
-    return l
-
-def get_list_exists(l:List, a):
-    ret = None
-    # 请在下一行编写代码，将判断a是否存在于l
-    ret = a in l
-    return ret
-
 if __name__ == "__main__":
     ret = get_variable_int()
