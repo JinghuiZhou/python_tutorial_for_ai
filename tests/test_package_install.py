7f13fd38c1cf38feef1a5c30e16cb1a3890ff70c
zhoujinghui <neo350942>
2023-03-15 21:28:50 +0800

    feat(package): add package import and unit tests

---
+++ tests/test_package_install.py
@@ -0,0 +1,51 @@
+import pytest
+
+
+def test_cv2():
+    import cv2
+
+def test_numpy():
+    import numpy
+
+def test_onnxruntime():
+    import onnxruntime
+
+def test_onnx():
+    import onnx
+
+def test_flask():
+    import flask
+
+def test_tqdm():
+    import tqdm
+
+def test_pygame():
+    import pygame
+
+def test_pyqt5():
+    import PyQt5
+
+def test_labelme():
+    import labelme
+
+def test_labelImg():
+    import labelImg
+
+def test_matplotlib():
+    import matplotlib
+
+def test_pil():
+    import PIL
+
+def test_wandb():
+    import wandb
+
+def test_tensorboard():
+    import tensorboard
+
+def test_scikit_image():
+    import skimage
+
+def test_mmcv():
+    import mmcv
+
